117f80ca58bb47898b332fb2f4f56d0de4f43cc1
André Lazarini <andre@servofiel.com.br>
2025-06-22 04:14:34 -0300

    add usuario administrador e empresa

---
+++ migrations/20250622063112-create-admin-user.js
@@ -0,0 +1,21 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash('admin123', 10);
+
+    await queryInterface.bulkInsert('usuarios', [{
+      nome: 'Administrador do Sistema',
+      email: 'admin@vagaqui.com',
+      senha: hashedPassword,
+      tipo_usuario: 'Administrador'
+    }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('usuarios', {
+      email: 'admin@vagaqui.com'
+    }, {});
+  }
+};
