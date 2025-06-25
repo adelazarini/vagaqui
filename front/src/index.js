d78414bc6d731687a0aac3c4a3b145a219108701
André Lazarini <andre@servofiel.com.br>
2025-06-25 02:25:03 -0300

    configurando debug fornt

--- front/src/index.js
+++ front/src/index.js
@@ -1,15 +1,15 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/auth_context';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
