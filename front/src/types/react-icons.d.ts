f7bc606e7b00c7f25871db0923e037f22a890133
André Lazarini <andre@servofiel.com.br>
2025-06-30 01:20:03 -0300

    ajusres erro compilacao

---
+++ front/src/types/react-icons.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-icons/fa' {
+    import { ComponentType } from 'react';
+
+    export const FaTrash: ComponentType<any>;
+    export const FaUserPlus: ComponentType<any>;
+    export const FaPlus: ComponentType<any>;
+    export const FaCheckCircle: ComponentType<any>;
+    export const FaBriefcase: ComponentType<any>;
+    export const FaUsers: ComponentType<any>;
+}
